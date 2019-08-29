87212bdc49889dc625d4abd4148c77bfbc168206
Paweł Gajda <pawegio@gmail.com>
2019-08-29 16:51:22 +0200

    Add border

--- library/src/main/java/pl/elpassion/debugtoolk/DebugComposable.kt
+++ library/src/main/java/pl/elpassion/debugtoolk/DebugComposable.kt
@@ -9,6 +9,8 @@ import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.foundation.shape.DrawShape
 import androidx.ui.foundation.shape.RectangleShape
+import androidx.ui.foundation.shape.border.Border
+import androidx.ui.foundation.shape.border.DrawBorder
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Align
 import androidx.ui.layout.Alignment
@@ -41,16 +43,21 @@ fun DebugComposable(any: Any?) {
 private fun AnyLog(any: Any?, depth: Int = 0) {
     println("dtk $any")
     Align(Alignment.TopLeft) {
-        val lightness = (0.4f + depth * 0.02f) % 1f
-        DrawShape(RectangleShape, Color(ColorUtils.HSLToColor(floatArrayOf(190f, 0.6f, lightness))))
-        when (any) {
-            null -> TextLog("null")
-            is List<*> -> CollectionLog(any, depth + 1)
-            is Number -> TextLog("number: $any")
-            is String -> TextLog("string: $any")
-            else -> {
-                val properties = any::class.declaredMemberProperties
-                AnyLog(properties.map { it.getter.call(any) }, depth)
+        val lightness = (0.6f + depth * 0.03f) % 1f
+        val color = Color(ColorUtils.HSLToColor(floatArrayOf(190f, 0.6f, lightness)))
+        val borderColor = Color(ColorUtils.HSLToColor(floatArrayOf(190f, 0.6f, 0.3f)))
+        DrawShape(RectangleShape, color)
+        DrawBorder(RectangleShape, Border(borderColor, 0.5.dp))
+        Padding(left = 4.dp, top = 4.dp, bottom = 4.dp) {
+            when (any) {
+                null -> TextLog("null")
+                is List<*> -> CollectionLog(any, depth + 1)
+                is Number -> TextLog("number: $any")
+                is String -> TextLog("string: $any")
+                else -> {
+                    val properties = any::class.declaredMemberProperties
+                    AnyLog(properties.map { it.getter.call(any) }, depth)
+                }
             }
         }
     }
