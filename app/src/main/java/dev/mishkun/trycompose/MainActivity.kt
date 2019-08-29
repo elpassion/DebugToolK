ce4d0f6e9977e9ec51b22239829f1cef4b9e03bc
Paweł Gajda <pawegio@gmail.com>
2019-08-29 14:47:50 +0200

    Introduce reflection to access properties

--- app/src/main/java/dev/mishkun/trycompose/MainActivity.kt
+++ app/src/main/java/dev/mishkun/trycompose/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("FunctionName")
+
 package dev.mishkun.trycompose
 
 import android.os.Bundle
@@ -10,6 +12,7 @@ import androidx.ui.layout.VerticalScroller
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.themeTextStyle
 import pl.elpassion.debugtoolk.DebugComposable
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,10 +26,31 @@ class MainActivity : AppCompatActivity() {
                             text = "Hello from MainActivity",
                             style = +themeTextStyle { h4 })
                     }
-                    val logs = List(10) { List(3) { List(2) { "Log $it" } } }
-                    DebugComposable(logs)
+                    val c = List(3) { C(RandomString(), Random.nextInt(), Random.nextFloat()) }
+                    val b = List(2) { B(RandomString(), c) }
+                    val a = List(5) { A(RandomString(), RandomString(), b) }
+                    DebugComposable(a)
                 }
             }
         }
     }
 }
+
+private fun RandomString(): String {
+    val leftLimit = 97 // letter 'a'
+    val rightLimit = 122 // letter 'z'
+    val targetStringLength = Random.nextInt(3, 15)
+    val buffer = StringBuilder(targetStringLength)
+    for (i in 0 until targetStringLength) {
+        val randomLimitedInt =
+            leftLimit + (Random.nextFloat() * (rightLimit - leftLimit + 1)).toInt()
+        buffer.append(randomLimitedInt.toChar())
+    }
+    return buffer.toString()
+}
+
+data class A(val a: String, val b: String, val c: List<B>)
+
+data class B(val a: String, val b: List<C>)
+
+data class C(val a: String, val b: Int, val c: Float)
--- library/src/main/java/pl/elpassion/debugtoolk/DebugComposable.kt
+++ library/src/main/java/pl/elpassion/debugtoolk/DebugComposable.kt
@@ -15,13 +15,18 @@ import androidx.ui.layout.Column
 import androidx.ui.layout.CrossAxisAlignment
 import androidx.ui.layout.Padding
 import androidx.ui.layout.VerticalScroller
+import androidx.ui.material.MaterialColors
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.themeColor
 import androidx.ui.material.themeTextStyle
-import androidx.ui.text.TextStyle
+import kotlin.reflect.KProperty1
+import kotlin.reflect.full.memberProperties
 
 @Composable
 fun DebugComposable(any: Any?) {
-    MaterialTheme {
+    MaterialTheme(
+        colors = MaterialColors(background = Color(0xFFCFD8DC.toInt()))
+    ) {
         VerticalScroller {
             Column {
                 Text(text = "Hello from DebugComposable", style = +themeTextStyle { h5 })
@@ -37,23 +42,22 @@ fun DebugComposable(any: Any?) {
 private fun AnyLog(any: Any?) {
     Padding(4.dp) {
         Align(Alignment.TopLeft) {
-            DrawShape(RectangleShape, Color.DarkGray)
+            DrawShape(RectangleShape, +themeColor { background })
             Padding(4.dp) {
                 when (any) {
-                    is List<*> -> {
-                        Column(crossAxisAlignment = CrossAxisAlignment.Start) {
-                            Text(
-                                any.toString(),
-                                style = TextStyle(color = Color.White),
-                                maxLines = 1
-                            )
-                            any.forEach(::AnyLog)
-                        }
+                    null -> Text("null")
+                    is List<*> -> Column(crossAxisAlignment = CrossAxisAlignment.Start) {
+                        Text(
+                            any.toString(),
+                            style = +themeTextStyle { body2 },
+                            maxLines = 2
+                        )
+                        any.forEach(::AnyLog)
                     }
-                    else -> Text(
-                        any.toString(),
-                        style = TextStyle(color = Color.White)
-                    )
+                    else -> {
+                        val properties = any::class.memberProperties as List<KProperty1<Any, *>>
+                        properties.map { it.get(any) }.filterIsInstance<List<*>>().forEach(::AnyLog)
+                    }
                 }
             }
         }
