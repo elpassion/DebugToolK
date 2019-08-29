78a9db531dfcfffe1fe52089223e4fe990ef7d54
Paweł Gajda <pawegio@gmail.com>
2019-08-29 12:45:30 +0200

    Add AnyLog composable

--- app/src/main/java/dev/mishkun/trycompose/MainActivity.kt
+++ app/src/main/java/dev/mishkun/trycompose/MainActivity.kt
@@ -1,14 +1,17 @@
 package dev.mishkun.trycompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.ui.core.setContent
+import pl.elpassion.debugtoolk.DebugComposable
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { SimpleComposable() }
+        setContent {
+            val logs = List(30) { "Log $it" }
+            DebugComposable(logs)
+        }
     }
 }
--- library/src/main/java/pl/elpassion/debugtoolk/TestLibComposable.kt
+++ library/src/main/java/pl/elpassion/debugtoolk/TestLibComposable.kt
@@ -1,21 +1,45 @@
+@file:Suppress("FunctionName")
+
 package pl.elpassion.debugtoolk
 
 import androidx.compose.Composable
-import androidx.compose.unaryPlus
+import androidx.ui.core.Dp
+import androidx.ui.core.Draw
 import androidx.ui.core.Text
-import androidx.ui.material.MaterialTheme
-import androidx.ui.material.themeTextStyle
-
-
+import androidx.ui.core.toRect
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Align
+import androidx.ui.layout.Alignment
+import androidx.ui.layout.Column
+import androidx.ui.layout.CrossAxisAlignment
+import androidx.ui.layout.Padding
+import androidx.ui.layout.VerticalScroller
+import androidx.ui.painting.Paint
+import androidx.ui.text.TextStyle
 
 @Composable
 fun DebugComposable(any: Any?) {
-    LogAny(any)
+    VerticalScroller {
+        Column(crossAxisAlignment = CrossAxisAlignment.Start) {
+            (any as? List<*>)?.forEach { AnyLog(it.toString()) }
+        }
+    }
 }
 
 @Composable
-private fun LogAny(any: Any?) {
-    MaterialTheme {
-        Text(text = "And the any is: $any", style = +themeTextStyle { h4 })
+private fun AnyLog(text: String) {
+    Padding(Dp(4f)) {
+        Align(Alignment.TopLeft) {
+            Draw { canvas, parentSize ->
+                val paint = Paint().apply { this.color = Color.DarkGray }
+                canvas.drawRect(parentSize.toRect(), paint)
+            }
+            Padding(Dp(4f)) {
+                Text(
+                    text,
+                    style = TextStyle(color = Color.White)
+                )
+            }
+        }
     }
 }
