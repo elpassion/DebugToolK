9169b8826643e319ee3b91dba8d2038ed9355fc0
Paweł Gajda <pawegio@gmail.com>
2019-08-29 16:32:59 +0200

    Add depth impacting shape color

--- library/src/main/java/pl/elpassion/debugtoolk/DebugComposable.kt
+++ library/src/main/java/pl/elpassion/debugtoolk/DebugComposable.kt
@@ -4,6 +4,7 @@ package pl.elpassion.debugtoolk
 
 import androidx.compose.Composable
 import androidx.compose.unaryPlus
+import androidx.core.graphics.ColorUtils
 import androidx.ui.core.Text
 import androidx.ui.core.dp
 import androidx.ui.foundation.shape.DrawShape
@@ -17,7 +18,6 @@ import androidx.ui.layout.Padding
 import androidx.ui.layout.VerticalScroller
 import androidx.ui.material.MaterialColors
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.themeColor
 import androidx.ui.material.themeTextStyle
 import kotlin.reflect.full.declaredMemberProperties
 
@@ -38,28 +38,29 @@ fun DebugComposable(any: Any?) {
 }
 
 @Composable
-private fun AnyLog(any: Any?) {
+private fun AnyLog(any: Any?, depth: Int = 0) {
     println("dtk $any")
     Align(Alignment.TopLeft) {
-        DrawShape(RectangleShape, +themeColor { background })
+        val lightness = (0.4f + depth * 0.02f) % 1f
+        DrawShape(RectangleShape, Color(ColorUtils.HSLToColor(floatArrayOf(190f, 0.6f, lightness))))
         when (any) {
             null -> TextLog("null")
-            is List<*> -> CollectionLog(any)
+            is List<*> -> CollectionLog(any, depth + 1)
             is Number -> TextLog("number: $any")
             is String -> TextLog("string: $any")
             else -> {
                 val properties = any::class.declaredMemberProperties
-                AnyLog(properties.map { it.getter.call(any) })
+                AnyLog(properties.map { it.getter.call(any) }, depth)
             }
         }
     }
 }
 
 @Composable
-private fun CollectionLog(collection: Collection<*>) = Padding(left = 4.dp) {
+private fun CollectionLog(collection: Collection<*>, depth: Int) = Padding(left = 4.dp) {
     Column(crossAxisAlignment = CrossAxisAlignment.Start) {
         TextLog("collection (size: ${collection.size})")
-        collection.forEach(::AnyLog)
+        collection.forEach { any -> AnyLog(any, depth) }
     }
 }
 
